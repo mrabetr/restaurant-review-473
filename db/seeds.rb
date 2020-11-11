646f8188fe9ed60a2a8220093f340fea34c24bf2
Phelim Dunleavy <dunleavy.phelim@gmail.com>
2020-11-11 09:20:27 +0000

    add faker gem and seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+puts "destroying old restaurants"
+Restaurant.destroy_all
+
+puts "creating restaurants"
+50.times do
+  new_restaurant = Restaurant.create!(
+    name: Faker::Restaurant.name ,
+    address: Faker::Address.street_address,
+    stars: rand(1..5)
+  )
+
+  puts "created restaurant with id #{new_restaurant.id}"
+end
+
+puts "Finished!"
